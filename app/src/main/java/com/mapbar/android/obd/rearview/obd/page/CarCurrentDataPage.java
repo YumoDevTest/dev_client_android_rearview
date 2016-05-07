7e8dfa38de63566edc8a048518202304808350ee
zhuchen <zhuchen@mapbar.com>
2016-05-07 16:28:08 +0800

    当前数据和体检

---
+++ app/src/main/java/com/mapbar/android/obd/rearview/obd/page/CarCurrentDataPage.java
@@ -0,0 +1,18 @@
+package com.mapbar.android.obd.rearview.obd.page;
+
+import com.mapbar.android.obd.rearview.framework.model.AppPage;
+
+/**
+ * Created by THINKPAD on 2016/5/6.
+ */
+public class CarCurrentDataPage extends AppPage {
+    @Override
+    public void initView() {
+
+    }
+
+    @Override
+    public void setListener() {
+
+    }
+}
