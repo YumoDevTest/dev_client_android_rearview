aefecb4a4f9be37786d36f1bab5eee9ce5f87f2b
zhuchen <zhuchen@mapbar.com>
2016-05-09 14:28:48 +0800

    chg 修正运行环境

--- app/src/main/java/com/mapbar/android/obd/rearview/obd/page/CarDataPage.java
+++ app/src/main/java/com/mapbar/android/obd/rearview/obd/page/CarDataPage.java
@@ -1,22 +1,14 @@
 package com.mapbar.android.obd.rearview.obd.page;
 
+import android.support.v7.widget.RecyclerView;
 
-import android.os.Bundle;
-
-import com.mapbar.android.obd.rearview.R;
 import com.mapbar.android.obd.rearview.framework.model.AppPage;
 
 /**
  * Created by THINKPAD on 2016/5/6.
  */
 public class CarDataPage extends AppPage {
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.page_car_current_data_page);
-    }
-
+    private RecyclerView recy_list;
     @Override
     public void initView() {
 
