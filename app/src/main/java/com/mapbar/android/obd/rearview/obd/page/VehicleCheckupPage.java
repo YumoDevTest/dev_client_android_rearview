2baaa69902fc4235d4711853a3db461df0beaad1
zhuchen <zhuchen@mapbar.com>
2016-05-16 12:04:15 +0800

    UI

--- app/src/main/java/com/mapbar/android/obd/rearview/obd/page/VehicleCheckupPage.java
+++ app/src/main/java/com/mapbar/android/obd/rearview/obd/page/VehicleCheckupPage.java
@@ -10,10 +10,11 @@ import android.widget.GridView;
 import com.mapbar.android.obd.rearview.R;
 import com.mapbar.android.obd.rearview.framework.activity.AppPage;
 import com.mapbar.android.obd.rearview.framework.inject.annotation.ViewInject;
+import com.mapbar.android.obd.rearview.framework.log.Log;
+import com.mapbar.android.obd.rearview.framework.log.LogTag;
 import com.mapbar.android.obd.rearview.framework.manager.PhysicalManager;
 import com.mapbar.android.obd.rearview.obd.MainActivity;
 import com.mapbar.android.obd.rearview.obd.OBDSDKListenerManager;
-import com.mapbar.android.obd.rearview.framework.manager.PhysicalManager;
 import com.mapbar.android.obd.rearview.obd.adapter.CheckupGridAdapter;
 import com.mapbar.android.obd.rearview.obd.adapter.VehicleCheckupAdapter;
 import com.mapbar.obd.Manager;
@@ -78,14 +79,39 @@ public class VehicleCheckupPage extends AppPage implements View.OnClickListener 
                 super.onEvent(event, o);
                 switch (event) {
                     case Manager.Event.obdPhysicalConditionFailed:
+                        // 日志
+                        if (Log.isLoggable(LogTag.TEMP, Log.VERBOSE)) {
+                            Log.v(LogTag.TEMP, "obdPhysicalConditionFailed -->>");
+                            Log.v(LogTag.TEMP, "Object -->>" + o);
+                        }
                         break;
                     case Manager.Event.obdPhysicalCheckStart:
+                        // 日志
+                        if (Log.isLoggable(LogTag.TEMP, Log.VERBOSE)) {
+                            Log.v(LogTag.TEMP, "obdPhysicalCheckStart -->>");
+                            Log.v(LogTag.TEMP, "Object -->>" + o);
+                        }
                         break;
                     case Manager.Event.obdPhysicalCheckResult:
+                        // 日志
+                        if (Log.isLoggable(LogTag.TEMP, Log.VERBOSE)) {
+                            Log.v(LogTag.TEMP, "obdPhysicalCheckResult -->>");
+                            Log.v(LogTag.TEMP, "Object -->>" + o);
+                        }
                         break;
                     case Manager.Event.obdPhysicalCheckEnd:
+                        // 日志
+                        if (Log.isLoggable(LogTag.TEMP, Log.VERBOSE)) {
+                            Log.v(LogTag.TEMP, "obdPhysicalCheckEnd -->>");
+                            Log.v(LogTag.TEMP, "Object -->>" + o);
+                        }
                         break;
                     case PhysicalManager.EVENT_OBD_PHYSICAL_CHECK_PROGRESS:
+                        // 日志
+                        if (Log.isLoggable(LogTag.TEMP, Log.VERBOSE)) {
+                            Log.v(LogTag.TEMP, "EVENT_OBD_PHYSICAL_CHECK_PROGRESS -->>");
+                            Log.v(LogTag.TEMP, "Object -->>" + o);
+                        }
                         break;
                 }
             }
@@ -97,8 +123,6 @@ public class VehicleCheckupPage extends AppPage implements View.OnClickListener 
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.btn_start_physical:
-                PhysicalManager physicalManager = new PhysicalManager();
-                physicalManager.startExam();
                 PhysicalManager.getInstance().startExam();
                 break;
         }
