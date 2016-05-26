2fe7882d8c3dc9a7562443b8f68e095ce21ef8c1
liuyy <liuyy@mapbar.com>
2016-05-26 16:26:35 +0800

    chg 新增启动服务

--- app/src/main/java/com/mapbar/android/obd/rearview/framework/activity/CConfigs.java
+++
@@ -1,8 +0,0 @@
-package com.mapbar.android.obd.rearview.framework.activity;
-
-public class CConfigs {
-    public final static int VIEW_POSITION_NONE = -1;
-    public final static int VIEW_POSITION_MAIN = 1;
-
-    public final static int VIEW_FLAG_NONE = -1;
-}
---
+++ app/src/main/java/com/mapbar/android/obd/rearview/framework/control/OBDV3HService.java
@@ -0,0 +1,19 @@
+package com.mapbar.android.obd.rearview.framework.control;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by liuyy on 2016/5/26.
+ */
+public class OBDV3HService extends Service {
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+
+}
