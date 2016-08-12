08ecabfbee4172cdf67a43b24fb20f4dfd6f79b7
tianff <tianff@mapbar.com>
2016-08-12 10:42:19 +0800

    chg 更新sdk,此版本达迅后视镜解决白屏

--- app/src/main/java/com/mapbar/android/obd/rearview/obd/MainActivity.java
+++ app/src/main/java/com/mapbar/android/obd/rearview/obd/MainActivity.java
@@ -71,9 +71,7 @@ public class MainActivity extends BaseActivity {
     private boolean restart;
     private boolean testAppUpdate = false;
     private PopupWindow updatePopu;
-
     private Handler handler;
-
     private String logFilePath = "";
 
     public static MainActivity getInstance() {
