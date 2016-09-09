bbdd9484b5bce960bb781eb7dc911108ca9421be
zhangyf <zhangyf@mapbar.com>
2016-09-09 17:22:02 +0800

    fix bug

--- app/src/main/java/com/mapbar/android/obd/rearview/modules/external/CarStatusBean.java
+++ app/src/main/java/com/mapbar/android/obd/rearview/modules/external/CarStatusBean.java
@@ -80,4 +80,64 @@ public class CarStatusBean {
 
 //        is_open_sunroof = false;
     }
+
+    @Override
+    public String toString() {
+        return toString(this);
+    }
+
+    private static String toString(CarStatusBean bean) {
+
+        StringBuilder sb = new StringBuilder();
+        sb.append(String.format("中控锁是否支持: %s， ", bean.is_support_lock));
+        sb.append(String.format("中控锁是否锁: %s， ", bean.is_locked));
+
+        sb.append(String.format("是否支持天窗: %s， ", bean.is_support_sunroof));
+        sb.append(String.format("是否打开天窗: %s， ", bean.is_open_sunroof));
+
+        sb.append(String.format("是否支持后备箱: %s， ", bean.is_support_trunk));
+        sb.append(String.format("是否打开后备箱: %s， ", bean.is_open_trunk));
+
+        sb.append(String.format("是否支持 灯: %s， ", bean.is_support_lights));
+
+        sb.append(String.format("小灯: %s， ", bean.is_on_light_small));
+        sb.append(String.format("近光灯: %s， ", bean.is_on_light_near));
+        sb.append(String.format("远光灯: %s， ", bean.is_on_light_far));
+        sb.append(String.format("前雾灯: %s， ", bean.is_on_light_fog_front));
+        sb.append(String.format("后雾灯: %s， ", bean.is_on_light_fog_back));
+        sb.append(String.format("左转灯: %s， ", bean.is_on_light_turn_left));
+        sb.append(String.format("右转灯: %s， ", bean.is_on_light_turn_right));
+        sb.append(String.format("危险报警双闪: %s， ", bean.is_on_light_dangerous));
+        sb.append(String.format("总开关: %s， ", bean.is_on_light_switch));
+
+//        sb.append(String.format(": %s， ",bean.is_support_left_front_lock));
+        sb.append(String.format("是否支持 左前门锁状态: %s， ", bean.is_support_left_front_lock));
+        sb.append(String.format("是否锁 左前门锁: %s， ", bean.is_on_left_front_lock));
+        sb.append(String.format("是否支持 右前门锁状态: %s， ", bean.is_support_right_front_lock));
+        sb.append(String.format("是否锁 右前门锁: %s， ", bean.is_on_righ_front_lock));
+        sb.append(String.format("是否支持 左后门锁状态: %s， ", bean.is_support_left_back_lock));
+        sb.append(String.format("是否锁 左后门锁: %s， ", bean.is_on_left_back_lock));
+        sb.append(String.format("是否支持 右后门锁状态: %s， ", bean.is_support_right_back_lock));
+        sb.append(String.format("是否锁 右后门锁: %s， ", bean.is_on_right_back_lock));
+
+        sb.append(String.format("是否支持 左前门开关状态: %s， ", bean.is_support_left_front_door));
+        sb.append(String.format("是否打开 左前门: %s， ", bean.is_on_left_front_door));
+        sb.append(String.format("是否支持 右前开关状态: %s， ", bean.is_support_right_front_door));
+        sb.append(String.format("是否打开 右前门: %s， ", bean.is_on_righ_front_door));
+        sb.append(String.format("是否支持 左后门开关状态: %s， ", bean.is_support_left_back_door));
+        sb.append(String.format("是否打开 左后门: %s， ", bean.is_on_left_back_door));
+        sb.append(String.format("是否支持 右后门开关状态: %s， ", bean.is_support_right_back_door));
+        sb.append(String.format("是否打开 右后门: %s， ", bean.is_on_right_back_door));
+
+        sb.append(String.format("是否支持 左前门车窗状态: %s， ", bean.is_support_left_front_window));
+        sb.append(String.format("是否打开 左前门车窗: %s， ", bean.is_on_left_front_window));
+        sb.append(String.format("是否支持 右前车窗状态: %s， ", bean.is_support_right_front_window));
+        sb.append(String.format("是否打开 右前门车窗: %s， ", bean.is_on_righ_front_window));
+        sb.append(String.format("是否支持 左后门车窗状态: %s， ", bean.is_support_left_back_window));
+        sb.append(String.format("是否打开 左后门车窗: %s， ", bean.is_on_left_back_window));
+        sb.append(String.format("是否支持 右后门车窗状态: %s， ", bean.is_support_right_back_window));
+        sb.append(String.format("是否打开 右后门车窗: %s， ", bean.is_on_right_back_window));
+
+        return sb.toString();
+    }
 }
--- app/src/main/java/com/mapbar/android/obd/rearview/modules/external/ExternalManager.java
+++ app/src/main/java/com/mapbar/android/obd/rearview/modules/external/ExternalManager.java
@@ -55,6 +55,7 @@ public class ExternalManager {
             throw new NullPointerException();
         if (carStatusBean == null)
             throw new NullPointerException();
+        android.util.Log.d(TAG, "## " + carStatusBean.toString());
         ExternalBroadcast.postCarStatus(context, CarStatusBeanConvert.convertToBundle(carStatusBean));
     }
 }
