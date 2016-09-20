b93f54990c24cfd4f88b6a5d8802939667547565
zhangyh <zhangyh@mapbar.com>
2016-09-20 19:10:13 +0800

    改动车辆故障码提示

--- demo/src/main/java/com/mapbar/demo/VoiceControlDemoActivity.java
+++ demo/src/main/java/com/mapbar/demo/VoiceControlDemoActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 
 import java.util.Timer;
+import java.util.TimerTask;
 
 public class VoiceControlDemoActivity extends Activity {
     public static final String VOICE_ACTION = "mapbar.obd.intent.action.VOICE_CONTROL";
@@ -20,25 +21,25 @@ public class VoiceControlDemoActivity extends Activity {
         timer = new Timer();
     }
     public void startExamination(View view){
-//        timer.schedule(new TimerTask() {
-//            @Override
-//            public void run() {
-//                Intent intent = new Intent(VOICE_ACTION);
-//                intent.putExtra("command", commands[count%6]);
-//                if (android.os.Build.VERSION.SDK_INT >= 12) {
-//                    intent.setFlags(Intent.FLAG_INCLUDE_STOPPED_PACKAGES);//3.1以后的版本需要设置Intent.FLAG_INCLUDE_STOPPED_PACKAGES
-//                }
-//                sendBroadcast(intent);
-//                count++;
-//            }
-//        },0,2000);
-        Intent intent = new Intent(VOICE_ACTION);
-        intent.putExtra("command", 103000);
-        if (android.os.Build.VERSION.SDK_INT >= 12) {
-            intent.setFlags(Intent.FLAG_INCLUDE_STOPPED_PACKAGES);//3.1以后的版本需要设置Intent.FLAG_INCLUDE_STOPPED_PACKAGES
-        }
-        sendBroadcast(intent);
-        System.exit(0);
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(VOICE_ACTION);
+                intent.putExtra("command", 102000 + count % 9);
+                if (android.os.Build.VERSION.SDK_INT >= 12) {
+                    intent.setFlags(Intent.FLAG_INCLUDE_STOPPED_PACKAGES);//3.1以后的版本需要设置Intent.FLAG_INCLUDE_STOPPED_PACKAGES
+                }
+                sendBroadcast(intent);
+                count++;
+            }
+        }, 0, 2000);
+//        Intent intent = new Intent(VOICE_ACTION);
+//        intent.putExtra("command", 103000);
+//        if (android.os.Build.VERSION.SDK_INT >= 12) {
+//            intent.setFlags(Intent.FLAG_INCLUDE_STOPPED_PACKAGES);//3.1以后的版本需要设置Intent.FLAG_INCLUDE_STOPPED_PACKAGES
+//        }
+//        sendBroadcast(intent);
+//        System.exit(0);
 //        Toast.makeText(getApplicationContext(), "发送广播成功", Toast.LENGTH_SHORT).show();
     }
 }
