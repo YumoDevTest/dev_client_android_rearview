207ddb76bcc16289ed197a33a60efe217eeb915e
zhangyh <zhangyh@mapbar.com>
2016-09-22 15:53:54 +0800

    修改保养项bug

--- app/src/main/java/com/mapbar/android/obd/rearview/modules/checkup/UpkeepItemAdapter.java
+++ app/src/main/java/com/mapbar/android/obd/rearview/modules/checkup/UpkeepItemAdapter.java
@@ -12,21 +12,24 @@ import android.widget.TextView;
 import com.mapbar.android.obd.rearview.R;
 import com.mapbar.android.obd.rearview.lib.config.MyApplication;
 import com.mapbar.obd.MaintenanceTask;
+import com.mapbar.obd.foundation.log.LogUtil;
 
 import java.util.Arrays;
 import java.util.List;
 
 /**
  * Created by THINKPAD on 2016/5/25.
+ * 设置保养项显示的adapter
+ * 根据获取的保养
  */
 public class UpkeepItemAdapter<VH extends RecyclerView.ViewHolder> extends RecyclerView.Adapter {
 
     private String[] names = {"更换发动机机油", "发动机润滑油", "更换机油滤清器", "更换空气滤清器", "更换PCV滤清器", "更换燃油滤清器",
-            "更换燃油粗滤器", "更换燃油空气滤清器", "更换火花塞", "更换前桥齿轮油", "更换后桥齿轮油", "更换齿轮油", "更换制动液", "更换主减速器油",
-            "更换发动机皮带", "更换压缩机皮带", "更换转向助力皮带", "更换传动皮带套件", "更换发动机正时套件", "更换空调滤清器", "前制动器", "后制动器",
+            "更换燃油粗滤器", "更换燃油空气滤清器", "更换火花塞", "更换前桥齿轮油", "更换后桥齿轮油", "更换齿轮油", "更换整车制动液", "更换主减速器油",
+            "更换发电机皮带", "更换压缩机皮带", "更换转向助力皮带", "更换传动皮带套件", "更换发动机正时套件", "更换空调滤清器", "前制动器", "后制动器",
             "更换电瓶", "更换蓄电池", "更换三元催化器", "轮胎换位", "更换点火线", "更换离合器油", "更换燃油泵", "更换PDCC储液罐", "更换碳罐", "清洗油路",
             "更换分动器油", "更换差速器油", "清洗进气道", "更换轮毂轴承润滑脂", "清洗喷油嘴", "清洗节气门", "更换CVVT油道滤网", "更换前轮轴承", "更换水泵",
-            "更换冷却液", "更换转向助力液", "更换变速器油", "变速箱油（CVT）", "变速箱油（AT）", "变速箱油（MT）", "变速箱油（DCT）", "变速箱油（AMT）"};
+            "更换冷却液", "更换转向助力液", "更换变速器油", "更换变速箱油（CVT）", "更换变速箱油（AT）", "更换变速箱油（MT）", "更换变速箱油（DCT）", "更换变速箱油（AMT）"};
 
     private int[] icons = {R.drawable.img_upkeep1, R.drawable.img_upkeep1, R.drawable.img_upkeep2, R.drawable.img_upkeep3, R.drawable.img_upkeep3, R.drawable.img_upkeep4,
             R.drawable.img_upkeep4, R.drawable.img_upkeep4, R.drawable.img_upkeep5, R.drawable.img_upkeep6, R.drawable.img_upkeep6, R.drawable.img_upkeep6, R.drawable.img_upkeep7,
@@ -43,7 +46,10 @@ public class UpkeepItemAdapter<VH extends RecyclerView.ViewHolder> extends Recyc
     private List nameList;
 
     public UpkeepItemAdapter(Context mContext, MaintenanceTask[] tasks) {
-
+        for (MaintenanceTask task :
+                tasks) {
+            LogUtil.d("UpkeepItemAdapter", "## " + task.name);
+        }
         this.tasks = tasks;
         this.mContext = mContext;
         nameList = Arrays.asList(names);
@@ -59,7 +65,10 @@ public class UpkeepItemAdapter<VH extends RecyclerView.ViewHolder> extends Recyc
 
         Holder holder = (Holder) arg0;
         int i = nameList.indexOf(tasks[position].name);
-        holder.img_icon.setImageBitmap(BitmapFactory.decodeResource(MyApplication.getInstance().getResources(), icons[i]));
+        if (i == -1)
+            holder.img_icon.setImageBitmap(BitmapFactory.decodeResource(MyApplication.getInstance().getResources(), icons[0]));
+        else
+            holder.img_icon.setImageBitmap(BitmapFactory.decodeResource(MyApplication.getInstance().getResources(), icons[i]));
         holder.tv_name.setText(String.valueOf(tasks[position].name));
     }
 
